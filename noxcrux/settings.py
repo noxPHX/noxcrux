36774f2997836d277f839e920c742e1a4e3b281e
Manu <nox@pop-os>
2020-12-29 18:46:44 +0100

    Throttling policy

--- noxcrux/settings.py
+++ noxcrux/settings.py
@@ -113,6 +113,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/day',
+        'user': '1000/day'
+    }
 }
 
 # Internationalization
