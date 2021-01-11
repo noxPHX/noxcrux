52e3b032d3a1b377879807ada8111a9f2c26aea8
Manu <nox@pop-os>
2021-01-11 18:19:08 +0100

    Messages fixed following bootstrap downgrade

--- noxcrux_server/static/js/clipboard.js
+++ noxcrux_server/static/js/clipboard.js
@@ -1,9 +1,9 @@
 $(".horcrux-copy").on('click', function () {
     try {
         navigator.clipboard.writeText($(this).find(".card-text").text());
-        $("#messages").append('<div class="alert-fixed"> <div class="alert alert-info alert-dismissible fade show me-3" role="alert"> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' + $(this).find(".card-text").data('name') + ' horcrux copied! </div> </div>')
+        $("#messages").append('<div class="alert alert-info alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + $(this).find(".card-text").data('name') + ' horcrux copied!</div>')
     } catch {
-        $("#messages").append('<div class="alert-fixed"> <div class="alert alert-warning alert-dismissible fade show me-3" role="alert"> <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> Clipboard access denied! Please copy manually </div> </div>')
+        $("#messages").append('<div class="alert alert-warning alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Clipboard access denied! Please copy manually</div>')
     }
     timeOuts();
 });
