00da41912cf8bdffd21ddfd666c40f96aafa9bf9
Manu <nox@pop-os>
2021-02-07 22:29:21 +0100

    Friend requests list

--- noxcrux_server/views/Friends.py
+++ noxcrux_server/views/Friends.py
@@ -1,16 +1,21 @@
 from noxcrux_server.mixins.Authenticated import LoginRequiredListView, LoginRequiredView
-from noxcrux_api.views.Friend import FriendList
+from noxcrux_api.views.Friend import FriendList, FriendRequest
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
 
 
 class FriendsView(LoginRequiredListView):
     template_name = 'friends.html'
     context_object_name = 'friends'
 
-    def get_queryset(self):
-        return FriendList().get(self.request).data
+    def get(self, request, *args, **kwargs):
+        context = {
+            'friends': FriendList().get(request).data,
+            'requests': FriendRequest().get(request).data
+        }
+        return render(request, self.template_name, context)
 
 
 class FriendDelete(LoginRequiredView):
