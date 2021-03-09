9374c4bc991bc783c423af0f7bca79dfc8bf14be
Manu <nox@pop-os>
2021-03-09 21:23:49 +0100

    Unshare horcruxes on friend remove

--- noxcrux_api/signals/friend.py
+++ noxcrux_api/signals/friend.py
@@ -8,3 +8,7 @@ def delete_friendship(sender, instance, using, **kwargs):
     reverse_friendship = Friend.objects.filter(user=instance.friend, friend=instance.user)
     if reverse_friendship:
         reverse_friendship.delete()
+
+    # Also delete shared horcrux between the two users
+    [horcrux.grantees.remove(instance.user) for horcrux in instance.user.shared_horcruxes.filter(owner=instance.friend)]
+    [horcrux.grantees.remove(instance.friend) for horcrux in instance.friend.shared_horcruxes.filter(owner=instance.user)]
