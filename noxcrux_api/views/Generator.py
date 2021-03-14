dccb139fb02e8b4ca3cfdb331a449bbd8691562c
Manu <nox@pop-os>
2021-03-14 11:09:25 +0100

    Generator

--- noxcrux_api/views/Generator.py
+++ noxcrux_api/views/Generator.py
@@ -1,31 +1,24 @@
-from rest_framework import status
 from noxcrux_api.serializers.Generator import GeneratorSerializer
 from noxcrux_api.models.Generator import Generator
 from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateAPIView
 
 
-class GeneratorDetail(APIView):
+class GeneratorDetail(RetrieveUpdateAPIView):
     """
-    Generate a horcrux or update the preferences
-    """
+    get:
+    Get a generated random horcrux based on preferences.
 
-    def get_object(self, owner):
+    put:
+    Update the generator settings.
+    """
+    serializer_class = GeneratorSerializer
+
+    def get_object(self):
+        return self.get_generator(self.request.user)
+
+    def get_generator(self, owner):
         try:
             return Generator.objects.get(owner=owner)
         except Generator.DoesNotExist:
             raise Http404
-
-    def get(self, request):
-        generator = self.get_object(request.user)
-        serializer = GeneratorSerializer(generator)
-        return Response(serializer.data)
-
-    def put(self, request):
-        generator = self.get_object(request.user)
-        serializer = GeneratorSerializer(generator, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- noxcrux_server/views/Generator.py
+++ noxcrux_server/views/Generator.py
@@ -12,19 +12,20 @@ class Generator(LoginRequiredFormView):
 
     def get_initial(self):
         initial = super(Generator, self).get_initial()
-        generator = GeneratorDetail().get_object(self.request.user)
+        generator = GeneratorDetail().get_generator(self.request.user)
         initial.update({'lower': True, 'generated': generator.generate()})
         return initial
 
     def get_form_kwargs(self):
         kwargs = super(Generator, self).get_form_kwargs()
-        generator = GeneratorDetail().get_object(self.request.user)
+        generator = GeneratorDetail().get_generator(self.request.user)
         kwargs.update({'instance': generator})
         return kwargs
 
     def form_valid(self, form):
         self.request.data = form.cleaned_data
-        res = GeneratorDetail().put(self.request)
+        self.request.method = 'PUT'
+        res = GeneratorDetail().as_view()(self.request)
         if res.status_code == 200:
             return super(Generator, self).form_valid(form)
         else:
