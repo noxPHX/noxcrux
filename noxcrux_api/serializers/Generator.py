1a732ab53fb941bb15f3a5dd94386bb5c9e59e43
Manu <nox@pop-os>
2021-03-17 19:35:21 +0100

    Warning removed

--- noxcrux_api/serializers/Generator.py
+++ noxcrux_api/serializers/Generator.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from noxcrux_api.models.Generator import Generator
+from drf_spectacular.utils import extend_schema_field
 
 
 class GeneratorSerializer(ModelSerializer):
@@ -9,5 +10,6 @@ class GeneratorSerializer(ModelSerializer):
         model = Generator
         exclude = ['id', 'owner']
 
+    @extend_schema_field(str)
     def get_generated_horcrux(self, generator):
         return generator.generate()
