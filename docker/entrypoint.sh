4779b2a7656d7319eed439af6fbaea6a0586ce71
Manu <nox@pop-os>
2021-05-09 15:24:55 +0200

    noxcrux_web dropped capabilities

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 python3 manage.py migrate
-chown -R "$USER:$USER" static/
 python3 manage.py collectstatic --no-input --clear --verbosity 0
 exec gunicorn --bind 0.0.0.0:8000 noxcrux.wsgi
