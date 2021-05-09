8d7d53e23a3a48149e27d80ea8d98bcc97bc042a
Manu <nox@pop-os>
2021-05-09 14:53:46 +0200

    SCSS compiled in multi stage build

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-chown -R "$USER:$USER" static/
-
 python3 manage.py migrate
-
-./scripts/compile_scss.sh
+chown -R "$USER:$USER" static/
 python3 manage.py collectstatic --no-input --clear --verbosity 0
-
 exec gunicorn --bind 0.0.0.0:8000 noxcrux.wsgi
