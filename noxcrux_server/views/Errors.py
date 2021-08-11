7a3779bc425e326681ca6d8e277ffe01f76d0cda
Manu <emmanuel.feral@protonmail.com>
2021-08-11 16:16:35 +0200

    Custom 500 view fix

--- noxcrux_server/views/Errors.py
+++ noxcrux_server/views/Errors.py
@@ -5,5 +5,5 @@ def error_404(request, exception):
     return render(request, '404.html', status=404)
 
 
-def error_500(request, exception):
+def error_500(request):
     return render(request, '500.html', status=500)
