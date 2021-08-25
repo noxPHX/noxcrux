d1e0b9cea95e2bd69979caf73ca40613abaed677
Manu <emmanuel.feral@protonmail.com>
2021-08-25 13:46:23 +0200

    CSP fix for Swagger UI

--- noxcrux/settings_csp.py
+++ noxcrux/settings_csp.py
@@ -7,15 +7,17 @@ CSP_CONNECT_SRC = ["'self'"]
 CSP_STYLE_SRC = [
     "'self'",
     "https://cdnjs.cloudflare.com",
-    "https://cdn.jsdelivr.net"
+    "https://cdn.jsdelivr.net",
+    "http://unpkg.com"
 ]
 
 CSP_SCRIPT_SRC = [
     "'self'",
     "https://code.jquery.com",
-    "https://cdn.jsdelivr.net"
+    "https://cdn.jsdelivr.net",
+    "http://unpkg.com"
 ]
 
-CSP_IMG_SRC = ["'self'"]
+CSP_IMG_SRC = ["'self' data:"]
 
 CSP_FONT_SRC = ["https://cdnjs.cloudflare.com"]
--- noxcrux_server/views/Swagger.py
+++ noxcrux_server/views/Swagger.py
@@ -1,5 +1,10 @@
 from drf_spectacular.views import SpectacularSwaggerView
+from csp.decorators import csp_update
 
 
 class APIDocView(SpectacularSwaggerView):
     template_name = 'swagger_ui.html'
+
+    @csp_update(SCRIPT_SRC="'unsafe-inline'")
+    def get(self, request, *args, **kwargs):
+        return super(APIDocView, self).get(self.request, *self.args, **self.kwargs)
