fbdf568d1ffc77464008cbc199cc50c33bec9e76
noxPHX <emmanuel.feral@protonmail.com>
2022-03-16 14:32:00 +0100

    Tests User list

---
+++ noxcrux_api/tests/test_user/test_create.py
@@ -0,0 +1,50 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from noxcrux_api.views.User import UserList
+from django.test.utils import override_settings
+
+
+class TestUserCreate(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.data = {'username': 'new', 'password': 'new'}
+        cls.url = reverse('users')
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestUserCreate, cls).setUpClass()
+        UserList.throttle_classes = ()
+
+    def test_create_user(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+
+    @override_settings(REGISTRATION_OPEN=False)
+    def test_create_user_registration_closed(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_create_user_duplicate_username(self):
+        self.client.post(self.url, self.data)
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_create_user_empty_username(self):
+        data = self.data.copy()
+        data['username'] = ''
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_create_user_empty_password(self):
+        data = self.data.copy()
+        data['password'] = ''
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
