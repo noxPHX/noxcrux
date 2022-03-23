b3f31101f62442fc8cb009ca285a1929140654b9
noxPHX <emmanuel.feral@protonmail.com>
2022-03-23 10:47:30 +0100

    Tests user token

--- noxcrux_api/urls.py
+++ noxcrux_api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('user/password/', PasswordUpdate.as_view(), name='api-password'),
     path('user/sessions/', UserSessionList.as_view(), name='api-sessions'),
     path('user/sessions/<str:session>/', UserSessionRevoke.as_view(), name='api-sessions-revoke'),
-    path('user/token/', UserToken.as_view()),
+    path('user/token/', UserToken.as_view(), name='api-sessions-token'),
     path('horcruxes/', HorcruxList.as_view(), name='horcruxes'),
     path('horcruxes/search/<str:search>/', HorcruxSearch.as_view()),
     path('horcruxes/granted/', HorcruxGrantedList.as_view()),
