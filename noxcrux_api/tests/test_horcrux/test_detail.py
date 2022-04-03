06c3296ee47a457589b153f4aafe4f52c9013685
noxPHX <emmanuel.feral@protonmail.com>
2022-04-03 15:13:54 +0200

    Test horcrux detail

--- noxcrux_api/tests/test_horcrux/test_detail.py
+++ noxcrux_api/tests/test_horcrux/test_detail.py
@@ -45,6 +45,11 @@ class TestHorcruxDetail(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual('Google', response.data['name'])
 
+    def test_get_horcrux_not_found(self):
+        self.client.force_login(self.test_user)
+        response = self.client.get(reverse('api-horcrux', args=('Youtube',)))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_put_horcrux_name(self):
         self.client.force_login(self.test_user)
         data = self.data.copy()
@@ -75,6 +80,11 @@ class TestHorcruxDetail(APITestCase):
         data['site'] = 'youtube'
         response = self.client.put(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_delete(self):
+        self.client.force_login(self.test_user)
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_not_allowed_post(self):
         self.client.force_login(self.test_user)
