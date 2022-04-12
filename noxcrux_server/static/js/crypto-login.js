b7b55eb211437d2af0934de1cd6adb5dd4f4b754
noxPHX <emmanuel.feral@protonmail.com>
2022-04-12 16:00:02 +0200

    Using master hash as password

--- noxcrux_server/static/js/crypto-login.js
+++ noxcrux_server/static/js/crypto-login.js
@@ -9,16 +9,7 @@ $("form").on('submit', async function (e) {
 
     let masterHash = await pbkdf2(fromUtf8(masterKey.b64), masterPassword, 30000, 256);
 
-    let keyPair = await generateRsaKeyPair();
-
-    let iv = window.crypto.getRandomValues(new Uint8Array(12))
-    let protectedKey = await encryptKey(masterKey, iv, keyPair.privateKey);
-
-    $("#id_public_key").val(keyPair.publicKey.b64);
-    $("#id_private_key").val(protectedKey.b64);
-    $("#id_iv").val(new ByteData(iv).b64);
     $('input[name="password"]').val(masterHash.b64);
-    $('input[name="password2"]').val(masterHash.b64);
 
     $(this).off('submit');
     $(this).submit();
