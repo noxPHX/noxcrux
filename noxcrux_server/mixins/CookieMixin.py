3658cd53b98d3c43755f134fdeb706e84ab93f18
noxPHX <emmanuel.feral@protonmail.com>
2022-04-21 15:15:58 +0200

    Secure cookies

--- noxcrux_server/mixins/CookieMixin.py
+++ noxcrux_server/mixins/CookieMixin.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 class CookieMixin(object):
 
     def __init__(self, *args, **kwargs):
@@ -23,5 +25,5 @@ class CookieMixin(object):
         """
         response = super(CookieMixin, self).dispatch(request, *args, **kwargs)
         for cookie_args, cookie_kwargs in self.get_cookies():
-            response.set_cookie(*cookie_args, **cookie_kwargs)
+            response.set_cookie(*cookie_args, **cookie_kwargs, httponly=True, samesite="Lax", secure=settings.SESSION_COOKIE_SECURE)
         return response
