64fd1b37fc58a4377702911ca3a5f7df1b4432ca
noxPHX <emmanuel.feral@protonmail.com>
2022-04-30 20:41:32 +0200

    Remove IndexedDB on logout

--- noxcrux_server/static/js/main.js
+++ noxcrux_server/static/js/main.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
         $('[data-toggle="popover"]').popover()
         $(".error-popover").popover('show');
     });
+
+    $("#id_logout").on('click', function () {
+
+        let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+        indexedDB.deleteDatabase('KeysContainer');
+    });
 });
